28926838b5e8c27ecd0ef8878bd02f28368ef080
GerardoG <zeus12gerardo@gmail.com>
2020-04-11 16:51:12 -0500

    Initial Commit

---
+++ PokeJerry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeJerry
+//
+//  Created by Jerry Gordillo on 11/04/20.
+//  Copyright © 2020 JerriodasCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
